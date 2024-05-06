58e889dea2c9c5fc95fddb693882b949a2d38a50
wuwenyu <johnnywwy@gmail.com>
2024-05-06 11:16:44 +0800

    feat：实现初始化

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  extends: [
+    require.resolve('eslint-config-airbnb'),
+    require.resolve('eslint-config-airbnb/hooks'),
+    require.resolve('eslint-config-airbnb-typescript'),
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    'react/function-component-definition': 0,
+    'react/react-in-jsx-scope': 0,
+    'import/prefer-default-export': 0,
+    // 'react-hooks/exhaustive-deps': 1,
+    "react-hooks/exhaustive-deps": "off"
+  },
+  ignorePatterns: [".eslintrc.cjs", "vite.config.ts"],
+  parser: require.resolve('@typescript-eslint/parser'),
+  parserOptions: {
+    project: require.resolve('./tsconfig.json'),
+  },
+  settings: {
+    react: {
+      'version': 'detect'
+    },
+    // 'import/resolver': {
+    //   'alias': {
+    //     'map': [
+    //       [
+    //         '@', './src'
+    //       ]
+    //     ],
+    //     'extensions': ['.ts', '.tsx']
+    //   }
+    // }
+  }
+}
---
+++ .prettierrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  // 在箭头函数的参数周围是否加上括号，此处设置为避免加括号
+  // arrowParens: 'avoid',
+
+  //是否在对象字面量的括号与对象之间加上空格
+  // bracketSpacing: true,
+
+  //换行符的类型，此处设置为 LF（Unix风格的换行符）
+  endOfLine: 'lf',
+
+  //在JSX中的右尖括号是否与标签的末尾放在同一行
+  // jsxBracketSameLine: false,
+
+  //每行的最大字符数，超过这个数值会被格式化
+  printWidth: 100,
+
+  //控制Prettier是否打包文本，保持原样或者自动换行
+  // proseWrap: 'preserve',
+
+  // 是否在语句末尾添加分号
+  // semi: false,
+
+  // 是否使用单引号而非双引号
+  // singleQuote: true,
+
+  // 一个制表符相当于几个空格
+  tabWidth: 2,
+
+  // 是否使用制表符来缩进，此处设置为不使用
+  // useTabs: false,
+
+  // 是否在对象和数组最后一个元素后加上逗号，此处设置为使用ES5规范的逗号风格
+  // trailingComma: 'es5',
+
+  // 使用的解析器，此处设置为TypeScript。
+  // parser: 'typescript',
+}
